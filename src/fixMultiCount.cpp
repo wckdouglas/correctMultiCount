ab8208dbd108ebbc2eb69ef4513381779bad1f9d
wckdouglas <wckdouglas@gmail.com>
2016-05-16 10:31:25 -0500

    updated vignettes, and correct function descriptions

--- src/fixMultiCount.cpp
+++ src/fixMultiCount.cpp
@@ -97,6 +97,13 @@ int columnExist(stringlist neededName, stringlist existingNames, string dfname)
   return 0;
 }
 
+//' @useDynLib correctMultiCount
+//' @importFrom Rcpp evalCpp
+//' @exportPattern "^[[:alpha:]]+"
+//' @title correctCounts
+//' @description Assigning multiply-mapped reads to most-abundantly-mapped locus
+//' @param basecount: should be a dataframe for storing gene count from uniquely-mapped reads **Have to contain columns named as 'id' for gene_id and 'count' for counts
+//' @param   multiCount should be a dataframe for storing multiply-mapped reads and their mapped locus. Columns named as 'fragment_id' for storing read id  and 'gene_id' for storing gene_id
 //' @export
 // [[Rcpp::export]]
 DataFrame correctCounts(DataFrame baseCount, DataFrame multiCount)
